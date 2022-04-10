051d019d48854404b0e1d6b3c7a43a121ea2f737
rak626 <rakeshacot@gmail.com>
2022-04-10 14:33:11 +0530

    minify assets for production

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,9 @@
 let mix = require('laravel-mix');
+mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css');
+mix.js('resources/js/app.js', 'public/js/app.js').sass(
+    'resources/scss/app.scss',
+    'public/css/app.css'
+);
+
+mix.minify(['public/js/app.js', 'public/css/app.css']);
